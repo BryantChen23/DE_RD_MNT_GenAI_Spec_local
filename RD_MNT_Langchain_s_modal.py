2cf9a6f83fac80faadb00e38cb9b0acf32979777
BryantChen23 <32960094+BryantChen23@users.noreply.github.com>
2024-04-25 08:59:15 +0800

    Add files via upload
    add import file format

--- RD_MNT_Langchain_s_modal.py
+++ RD_MNT_Langchain_s_modal.py
@@ -2,14 +2,35 @@ import streamlit as st
 import os
 import tempfile
 
-from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import (
+    Docx2txtLoader,
+    PyPDFLoader,
+    CSVLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredExcelLoader,
+)
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 
 
-# Functions
+loader_map = {
+    ".doc": Docx2txtLoader,
+    ".docx": Docx2txtLoader,
+    ".pdf": PyPDFLoader,
+    ".csv": CSVLoader,
+    ".xlsx": UnstructuredExcelLoader,
+    ".pptx": UnstructuredPowerPointLoader,
+}
+
+
+# 取得檔案類型擴展名
+def get_file_tyep(filename):
+    _, file_type = os.path.splitext(filename)
+    return file_type.lower()
+
+
 def file_load(uploaded_files):
     """read document"""
     sup_filenames = []
@@ -24,13 +45,29 @@ def file_load(uploaded_files):
                 temp_file.write(file.getvalue())
                 loader = Docx2txtLoader(temp_path)
                 docs.extend(loader.load())
-
     return sup_filenames, docs
+
+
+def file_load_new(uploaded_files):
+    """read document"""
+    docs = []
+    temp_dir = tempfile.TemporaryDirectory()
+
+    for file in uploaded_files:
+        file_type = get_file_tyep(file.name)
+        if file_type in loader_map:
+            temp_path = os.path.join(temp_dir.name, file.name)
+            with open(temp_path, "wb") as temp_file:
+                temp_file.write(file.getvalue())
+            loader = loader_map[file_type]
+            docs.extend(loader(temp_path).load())
+
+    return docs
 
 
 def file_splitter(docs):
     """document split"""
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
     splits = splitter.split_documents(docs)
 
     return splits
@@ -60,14 +97,20 @@ st.title(
 
 # file selector
 uploaded_files = st.sidebar.file_uploader(
-    label="Select", type=["docx"], accept_multiple_files=True, label_visibility="hidden"
+    label="Select",
+    type=["docx", "pptx", "csv", "pdf", "xlsx"],
+    accept_multiple_files=True,
+    label_visibility="hidden",
 )
+
 
 if not uploaded_files:
     st.info("Please upload documents to continue.")
+    # 停頓點
     st.stop()
 
-sup_filenames, docs = file_load(uploaded_files)
+
+docs = file_load_new(uploaded_files)
 splits = file_splitter(docs)
 retriever = embedding_to_vector(splits)
 
@@ -83,27 +126,27 @@ retriever = embedding_to_vector(splits)
 # OpenAI
 llm = ChatOpenAI(model="gpt-4-1106-preview", api_key=os.getenv("OPENAI_API"))
 
-
+# Chain
 qa_chain = ConversationalRetrievalChain.from_llm(
-    llm=llm, return_source_documents=True, verbose=True, retriever=retriever
+    llm=llm, retriever=retriever, verbose=True
 )
 
 # Initialize st_chat history and create message container
 if "messages" not in st.session_state:
+    # Chat history
+    chat_history = []
     st.session_state.messages = []
     with st.chat_message("assistant"):
-        st.write("How can i help you?")
+        st.write("How can i help you today?")
 
 # Display chat messages from history
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Chat history
-chat_history = []
 
 # Accept user input
-if prompt := st.chat_input("Ask me anything!"):
+if prompt := st.chat_input(placeholder="Ask me anything!"):
 
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
